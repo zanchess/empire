d464ba3063c52ca4447fff85f8fde3a8901b6eca
Lashkevich Aliaksandr <a.lashkevich93@gmail.com>
2025-07-04 10:35:32 +0300

    init: init project

---
+++ backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/empire'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
